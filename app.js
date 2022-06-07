c8b9103698594ba7f40fb030c60e9d9a88bc8c62
Hathaway-Shan <hathaway.shan@gmail.com>
2022-06-07 14:54:27 -0700

    added utils.js

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 // import needed modules
-
+import { getRandomItem, score } from './utils.js';
 // state
+const throws = ['rock', 'paper', 'scissors']
+let games = 0;
+let wins = 0;
+let losses = 0;
+let draws = 0;
+let timeout = 0;
+
+
 
 // components
+
     // component
     // define and grab DOM elements
+
     // display functions
     // optional: subscribe to events
         // event handlers - what needs to happen?
---
+++ utils.js
@@ -0,0 +1,22 @@
+export function getRandomItem(array) {
+    const random = Math.floor(Math.random() * array.length);
+    const item = array[random];
+    return item;
+}
+export function score(playerChoice, computerChoice) {
+    if (playerChoice === 'rock' && computerChoice === 'scissors') {
+        return 1;
+    } else if (playerChoice === 'scissors' && computerChoice === 'paper') {
+        return 1;
+    } else if (playerChoice === 'paper' && computerChoice === 'rock') {
+        return 1;
+    } else if (playerChoice === 'scissors' && computerChoice === 'rock') {
+        return 0;
+    } else if (playerChoice === 'paper' && computerChoice === 'scissors') {
+        return 0;
+    } else if (playerChoice === 'rock' && computerChoice === 'paper') {
+        return 0;
+    } else {
+        return 0;
+    } 
+}
